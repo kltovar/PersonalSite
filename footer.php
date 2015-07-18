5b55961a6c008a7be7502d5cba4c0bd955e3483c
Karina <kl.tovar@gmail.com>
2015-07-18 13:43:29 -0400

    Start repository

---
+++ footer.php
@@ -0,0 +1,13 @@
+
+	<section id="footer">
+		<div></div>
+	</section>
+
+	</section> 
+	<!-- end SITE CONTAINER -->
+
+	<!-- Javascript -->
+	<script src="/_js/name.js"></script>
+	<script src="/_js/vendor/fastclick.js" async></script>
+    </body>
+</html>
---
+++ header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title><?php echo $pageTitle ?></title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+    <meta name="description" content="<?php echo $metaDescription ?>"></meta>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- html5shiv -->
+	<script src="/_js/vendor/html5shiv.js" async></script>
+	<script src="/_js/vendor/html5shiv-printshiv.js" async></script>
+
+    <!-- Pulled Stylesheets -->
+    <link rel="stylesheet" href="/_css/vendor/normalize.css"/>
+  	<link rel="stylesheet" href="/_css/vendor/skeleton.css"/>
+  	<link rel="stylesheet" href="/_css/vendor/animate.min.css"/>
+	<link rel="stylesheet" href="/_css/app.css"/>
+	<link rel="stylesheet" href="/_css/media-queries.css">
+	<!-- Scripts-->
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script src="/_js/smooth-scroll.js" defer="defer"></script>
+
+	<!-- GA Tracking Codes -->
+
+</head>
+
+<body>
+	
+	<!-- Start Site Container -->
+	<section id="site-container">
+
+		<!-- Navigation -->
+		<header>
+			<div>
+				
+			</div>
+		</header>
+		<!-- Navigation -->
+
+	
+
